9871f79fa8be2344438185f2fb3c9ed2c39b2074
unknown <tomasz.szewczyk1@microsoft.wsei.edu.pl>
2022-06-27 15:11:44 +0200

    Uporządkowanie gita + nowy projekt

--- ApplicationBook/src/main/java/tszewczyk/ApplicationBook/dao/BookRepo.java
+++
@@ -1,9 +0,0 @@
-package tszewczyk.ApplicationBook.dao;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import tszewczyk.ApplicationBook.dao.entity.Book;
-
-@Repository
-public interface BookRepo extends CrudRepository<Book, Long> {
-}
